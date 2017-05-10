3b0e5a832eb5aaf0c178117ae4a1698a276bfe65
taylor <taylorsilva@outlook.com>
2017-05-10 01:23:57 -0400

    A note on coupling

--- app/src/main/java/com/taylorsilva/shopify/Callback.java
+++ app/src/main/java/com/taylorsilva/shopify/Callback.java
@@ -2,6 +2,8 @@ package com.taylorsilva.shopify;
 
 /**
  * Created by taylor on 2017-05-09.
+ * This callback makes things very couple-y but alas,
+ * it's all I got for one days work! It got the job done!
  */
 
 public interface Callback {
